084faf73a543680e2f76af8e9f068f6471e14dee
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-05 09:49:03 +0200

    Add files via upload

---
+++ Tableau/Tableau/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+char[] charArray1 = { 'a', 'b', 'c', 'd' };
+
+char[] charArray2 = { 'e', 'f', 'g', 'h' };
+
+char[] charArray3 = new char[8];
+
+for (int i = 0; i < charArray1.Length; i++)
+{
+    charArray3[i] = charArray1[i];
+}
+
+for (int i = 0; i < charArray2.Length; i++)
+{
+    charArray3[i+3] = charArray2[i];
+}
+
+Console.WriteLine(charArray3);
